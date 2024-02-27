0ade6a3943a0adffdafc2769464b5eb1480e9a34
0cchang <calebschang13@gmail.com>
2024-02-26 23:10:48 -0500

    testing added level

---
+++ GeometryDashGame/src/game/Level.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Level extends Polygon{
+
+    public Level(Point[] inShape, Point inPosition, double inRotation) {
+        super(inShape, inPosition, inRotation);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
